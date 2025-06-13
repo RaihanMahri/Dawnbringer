eee2f68e91290deae372bed555680ae4c1fb34ed
RaihanMahri <raihanmahri123@gmail.com>
2025-06-14 05:04:58 +0700

    update

--- app/src/main/java/com/ubaya/dawnbringer/model/UserDao.kt
+++ app/src/main/java/com/ubaya/dawnbringer/model/UserDao.kt
@@ -4,16 +4,20 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.RewriteQueriesToDropUnusedColumns
 
 @Dao
 interface UserDao {
-    @Insert(onConflict = OnConflictStrategy.ABORT)
+    @Insert
     suspend fun insert(user: User)
 
     @Query("SELECT * FROM user WHERE username = :username")
+    @RewriteQueriesToDropUnusedColumns
     suspend fun getByUsername(username: String): User?
 
     @Query("SELECT * FROM user WHERE username = :username AND password = :password")
+    @RewriteQueriesToDropUnusedColumns
     suspend fun login(username: String, password: String): User?
 }
+
 
